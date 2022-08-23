6220574e9e3c01b46be8bb6c165b63715ecd0db4
SoulOriginal <57761401+SoulOriginal@users.noreply.github.com>
2022-08-23 19:36:07 +0200

    update npm molules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   meta: {
-    title: "Animakuro 1",
+    title: "VE",
   },
   srcDir: "./",
   app: {
