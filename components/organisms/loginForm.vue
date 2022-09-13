27428299d4edd44cc42ac249b81369410dfa9e27
joffyNewJong <nikikrokha@gmail.com>
2022-09-13 22:59:21 +0300

    added a input

---
+++ components/organisms/loginForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login-container">
+    <header><h1></h1></header>
+    <form>
+      <div>
+        <AtomsUiInput />
+        <AtomsUiInput />
+      </div>
+    </form>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.login-container {
+  max-width: 579px;
+  margin: 0 auto;
+  background: #131620;
+  padding: 42px;
+  border: 2px solid #494a4d;
+  border-radius: 16px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <OrganismsNavbar />
-  <OrganismsChooser />
-  <OrganismsFooter />
+  <OrganismsLoginForm />
 </template>
 
 <style lang="scss">
