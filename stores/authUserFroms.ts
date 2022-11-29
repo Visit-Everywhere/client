b2a1f8df0849ed3010af4848fc2788eaabf1405e
Slipmaks <slipmaks@gmail.com>
2022-11-29 23:32:20 +0200

    add types authUserForms

--- stores/authUserFroms.ts
+++ stores/authUserFroms.ts
@@ -1,12 +1,36 @@
 import { defineStore } from "pinia";
 
+interface userDto {
+  email: string;
+  id: string;
+  isActivated: boolean;
+  username: string;
+  userRoles: string[];
+}
+interface userResponse {
+  accessToken: string;
+  refreshToken: string;
+  userDto: userDto;
+}
 interface State {
   test: string;
+  currentUser: userResponse;
 }
 
 export const authUserState = defineStore("authUser", {
   state: (): State => ({
     test: "hello",
+    currentUser: {
+      accessToken: "",
+      refreshToken: "",
+      userDto: {
+        email: "",
+        id: "",
+        isActivated: false,
+        username: "",
+        userRoles: [],
+      },
+    },
   }),
   actions: {
     test(): void {
